be8780299cd6abb61b410071ef361049668b6936
Cezar Sirbu <cezar.sirbu.e@thalesdigital.io>
2019-12-12 18:55:57 +0200

    Add CORS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
